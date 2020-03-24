135dccd5b466b325bf2fb62795ae57f9e8a2dae8
mohamedisakr <mohamedisakr@gmail.com>
2020-03-24 14:05:50 +0200

    second commit

--- arrays/1365/problem.js
+++ arrays/1365/problem.js
@@ -1,3 +1,4 @@
+// my first implementation
 /**
  * Given the array nums, for each nums[i] find out how many numbers in the
  * array are smaller than it. That is, for each nums[i] you have to count
@@ -6,7 +7,11 @@
  * @param {number[]} nums
  * @return {number[]}
  */
+/*
 function smallerNumbersThanCurrent(nums) {
+  //   let sortedNumbers = nums.sort();
+  //   console.log(sortedNumbers);
+
   let result = [];
   for (let i = 0; i < nums.length; i++) {
     let count = 0;
@@ -19,5 +24,91 @@ function smallerNumbersThanCurrent(nums) {
   }
   return result;
 }
+// */
+// /*
+function smallerNumbersThanCurrent(nums) {
+  const sortedNumbers = nums.sort();
+  console.log(sortedNumbers);
 
-module.exports = smallerNumbersThanCurrent;
+  // define the resulting array
+  const result = [];
+  // [1, 2, 2, 3, 8]
+  // [0, 1, 1, 3, 4]
+  // for every item in sortedNumbers array
+
+  // npm run test
+  debugger;
+  for (let i = 0; i < sortedNumbers.length; i++) {
+    result.push(search(sortedNumbers, sortedNumbers[i]));
+  }
+  // return the resulting array
+  return result;
+}
+// */
+
+/**
+ *
+ * @param {*} list
+ * @param {*} key
+ */
+function search(list, key) {
+  let lo = 0;
+  let hi = list.length - 1;
+  if (key < list[lo] || key > list[hi]) {
+    return -1;
+  }
+  //alert('test');
+  while (lo <= hi) {
+    let mid = Math.floor((lo + hi) / 2);
+    if (key < list[mid]) {
+      hi = mid - 1;
+    } else if (key > list[mid]) {
+      lo = mid + 1;
+    } else {
+      return mid;
+    }
+  }
+  return -1;
+}
+
+module.exports = { smallerNumbersThanCurrent, search };
+
+// return {
+//   smallerNumbersThanCurrent: smallerNumbersThanCurrent,
+//   search: search
+// };
+// module.exports = smallerNumbersThanCurrent, search;
+// export default smallerNumbersThanCurrent;
+
+// */
+
+/*
+const smallerNumbersThanCurrent = nums => {
+  const map = new Map(
+    nums
+      .slice()
+      .sort((a, b) => a - b)
+      .map((val, idx) => [val, idx])
+      .reverse()
+  );
+  console.log(map);
+
+  return nums.map(n => map.get(n));
+};
+*/
+
+/*
+var smallerNumbersThanCurrent = function(nums) {
+  // create a new instance form nums and sort it
+  const sortedNums = Array.from(nums).sort((a, b) => a - b);
+  let result = []; // result array
+
+  //iterate through nums array
+  for (let num of nums) {
+    // find the index of the current number in the sorted array
+    let idx = sortedNums.findIndex(num); //.indexOf(num);
+    result.push(idx);
+  }
+  return result;
+};
+*/
--- arrays/1365/problem.test.js
+++ arrays/1365/problem.test.js
@@ -1,23 +1,57 @@
-// import { smallerNumbersThanCurrent } from "./problem.js";
-const smallerNumbersThanCurrent = require("./problem");
+// import { smallerNumbersThanCurrent, search } from "./problem.js";
+const problem = require("./problem.js");
 
-test("test case 1", () => {
-  const inputArray = [8, 1, 2, 2, 3];
-  const outputArray = [4, 0, 1, 1, 3];
-  const expectOutput = smallerNumbersThanCurrent(inputArray);
-  expect(expectOutput).toEqual(outputArray);
+describe("smallerNumbersThanCurrent", () => {
+  test("test case 1", () => {
+    const inputArray = [8, 1, 2, 2, 3];
+    const outputArray = [4, 0, 1, 1, 3];
+    const expectOutput = problem.smallerNumbersThanCurrent(inputArray);
+    expect(expectOutput).toEqual(outputArray);
+  });
+
+  test("test case 2", () => {
+    const inputArray = [6, 5, 4, 8];
+    const outputArray = [2, 1, 0, 3];
+    const expectOutput = problem.smallerNumbersThanCurrent(inputArray);
+    expect(expectOutput).toEqual(outputArray);
+  });
+
+  test("test case 3", () => {
+    const inputArray = [7, 7, 7, 7];
+    const outputArray = [0, 0, 0, 0];
+    const expectOutput = problem.smallerNumbersThanCurrent(inputArray);
+    expect(expectOutput).toEqual(outputArray);
+  });
+
+  test("test case 4", () => {
+    const inputArray = [1, 2, 2, 3, 8];
+    const outputArray = [0, 1, 1, 3, 4];
+    const expectOutput = problem.smallerNumbersThanCurrent(inputArray);
+    expect(expectOutput).toEqual(outputArray);
+  });
 });
 
-test("test case 2", () => {
-  const inputArray = [6, 5, 4, 8];
-  const outputArray = [2, 1, 0, 3];
-  const expectOutput = smallerNumbersThanCurrent(inputArray);
-  expect(expectOutput).toEqual(outputArray);
-});
+// ***************************************************************************************
 
-test("test case 3", () => {
-  const inputArray = [7, 7, 7, 7];
-  const outputArray = [0, 0, 0, 0];
-  const expectOutput = smallerNumbersThanCurrent(inputArray);
-  expect(expectOutput).toEqual(outputArray);
+describe("search", () => {
+  test("case 1: odd length", () => {
+    const inputArray = [1, 2, 2, 3, 8];
+    const key = 3;
+    const expectOutput = problem.search(inputArray, key);
+    expect(expectOutput).toEqual(3);
+  });
+
+  test("case 2: even length", () => {
+    const inputArray = [1, 2, 3, 4, 7, 8, 9, 10];
+    const key = 7;
+    const expectOutput = problem.search(inputArray, key);
+    expect(expectOutput).toEqual(4);
+  });
+
+  test("case 3: non existing", () => {
+    const inputArray = [1, 2, 3, 4, 7, 8, 9, 10];
+    const key = 100;
+    const expectOutput = problem.search(inputArray, key);
+    expect(expectOutput).toEqual(-1);
+  });
 });
